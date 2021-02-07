17966cad407420b5ce9a8b403b1f6ae08a17b32f
Daniel P. Egan <dp.egan@gmail.com>
2021-02-07 07:42:42 -0500

    more comments in cache prep file

--- julia_sysimage_precompile_prep.jl
+++ julia_sysimage_precompile_prep.jl
@@ -1,5 +1,6 @@
 import Pkg
 
+# Add the packages you want to precompile
 Pkg.add("PackageCompiler")
 Pkg.add("Plots")
 Pkg.add("OhMyREPL")
@@ -7,8 +8,9 @@ Pkg.add("DataFrames")
 Pkg.add("DataFramesMeta")
 Pkg.add("Chain")
 Pkg.add("Statistics")
-Pkg.update()
+Pkg.update() # Make sure they're up to date.
 
+# Open them up
 using PackageCompiler
 using OhMyREPL
 using Plots
@@ -17,10 +19,11 @@ using DataFrames
 using DataFramesMeta
 using Chain
 
+# You need to use a function from each package once to trigger compilation.
 1+1 # OhMyREPL
 plot(1:10, rand(10)) # Plots
 
-# DataFrames, DataFramesMeta, Chain
+# DataFrames, DataFramesMeta, Chain, Statistics
 df = DataFrames.DataFrame(x=["a", "a", "b", "b"], y=[1,2,3,4])
 @chain df begin
     groupby(:x)
